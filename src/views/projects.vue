cfa1e81cc36c1881a94027e578a1bbe688ff0680
Helene Andre <helene.andre.06@gmail.com>
2019-03-23 15:14:58 +1100

    Modify style projects slides.

--- src/views/projects.vue
+++ src/views/projects.vue
@@ -3,7 +3,7 @@
 		.section--wrapper
 			h1.projects__title PROJECTS
 			.slide--wrapper
-				vueper-slides.no-shadow(:slide-ratio="0.5" arrows-outside slide-content-outside="bottom" bullets-outside)
+				vueper-slides.no-shadow(:slide-ratio="0.5" arrows-outside slide-content-inside="bottom" bullets-outside)
 					vueper-slide(v-for="(slide, i) in slides"
 						:key="i"
 						:image="getImage(slide.image)"
@@ -22,7 +22,7 @@ export default {
     slides: [
       {
         title: 'Colors of the triangles',
-				content: `<div class="slide__content">
+				content: `<div slot="slide__content">
 										<p>Randomised sizes & colors, click to reveal the colors of the triangles.</p>
                 		<p><i class="icon icon-wand"></i> HTML, CSS, jQuery.</p>
 										<div class="project__link"><a href="https://codepen.io/Helene-Andre/pen/JxajwE" target="_blank">https://codepen.io/Helene-Andre/pen/qgXVya</a></div>
@@ -31,7 +31,7 @@ export default {
 			},
       {
         title: 'Pure CSS heart beating',
-				content: `<div class="slide__content">
+				content: `<div slot="slide__content">
 										<p>This is a pure CSS heart beating, spread the CSS love <i class="icon icon-love"></i></p>
 										<p><i class="icon icon-wand"></i> CSS only</p>
 										<div class="project__link"><a href="https://codepen.io/Helene-Andre/pen/qgXVya" target="_blank">https://codepen.io/Helene-Andre/pen/qgXVya</a></div>
@@ -40,7 +40,7 @@ export default {
 			},
 			{
         title: 'Pure CSS cat',
-				content: `<div class="slide__content">
+				content: `<div slot="slide__content">
 										<p>Click the box to reveal the cat.</p>
 										<p><i class="icon icon-wand"></i> HTML, CSS, Javascript.</p>
 										<div class="project__link"><a href="https://codepen.io/Helene-Andre/pen/mQrgZa" target="_blank">https://codepen.io/Helene-Andre/pen/mQrgZa</a></div>
@@ -49,7 +49,7 @@ export default {
 			},
 			{
 				title: 'CSS pupil tracks mouse',
-				content: `<div class="slide__content">
+				content: `<div slot="slide__content">
 										<p>The pupil moves on mouse move.</p>
 										<p><i class="icon icon-wand"></i> HTML, CSS, Javascript.</p>
 										<div class="project__link"><a href="https://codepen.io/Helene-Andre/pen/MzovxP" target="_blank">https://codepen.io/Helene-Andre/pen/MzovxP</a></div>
@@ -58,7 +58,7 @@ export default {
 			},
 			{
 				title: 'Momentum',
-				content: `<div class="slide__content">
+				content: `<div slot="slide__content">
 										<p>Momentum is a Chrome plugin for which I tried to replicate the design.</p>
 										<p><i class="icon icon-wand"></i> HTML, CSS, Javascript.</p>
 										<div class="project__link"><a href="https://helene-andre.github.io/momentum/ " target="_blank">https://helene-andre.github.io/momentum/ </a></div>
@@ -67,7 +67,7 @@ export default {
 			},
 			{
 				title: 'HTML Memento',
-				content: `<div class="slide__content">
+				content: `<div slot="slide__content">
 										<p>This is the first website I created in November 2018 to gather HTML tags.</p>
 										<p><i class="icon icon-wand"></i> HTML, CSS, Javascript.</p>
 										<div class="project__link"><a href="https://helene-andre.github.io/html-memento/" target="_blank">https://helene-andre.github.io/html-memento/</a></div>
@@ -94,15 +94,15 @@ export default {
 	}
 }
 
-.projects__title {
-	color: #ffffff;
-}
-
 .projects-wrapper {
   position: absolute;
   top: 50%; 
   transform: translateY(-50%);
   padding-top: 20px;
+}
+
+.projects__title {
+	color: #ffffff;
 }
 
 .project__link {
@@ -127,17 +127,25 @@ export default {
 }
 
 .vueperslide__content-wrapper {
-	color: #ffffff;
-	padding: 12px;
-	background-color: rgba(128,128,128, 0.8);	position: absolute; 
-	position: inherit;
-	-webkit-transform: translate(0%,0%);
-	transform: translate(0%,0%);
-	text-align: unset; 
+	width: 100%; 
+	color: #fff;
+	padding: 12px 0 12px 0;
+	background-color: rgba(128,128,128, 0.8);	
+	& div:nth-child(1) {
+		padding-left: 12px; 
+	}
+	& p {
+		color: #fff;
+	}
 }
 
-.vueperslide__content-wrapper p {
-	color: #ffffff;
+.vueperslide__content-wrapper:not(.vueperslide__content-wrapper--outside-top):not(.vueperslide__content-wrapper--outside-bottom) {
+    top: unset;
+		bottom: 0; 
+    left: 50%;
+    -webkit-transform: translate(-50%,0%);
+    transform: translate(-50%,0%);
+    text-align: left;
 }
 
 .vueperslide__title {

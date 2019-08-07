2268a2576492bbb59ecbd62793cbf914ad8a897a
Helene Andre <helene.andre.06@gmail.com>
2019-08-07 13:14:12 +1000

    Publish website to Github.

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,4 @@
 module.exports = {
 	outputDir: 'docs',
-	publicPath: process.env.NODE_ENV === 'production'
-	  ? '/portfolio'
-		: '/'
+	publicPath: process.env.NODE_ENV === 'production' ? '/portfolio-template' : '/'
 }
